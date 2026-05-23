a9c113138526245d47fad44358f375efc8965c41
Rahaf Mohammed 4872 <rahafaofa5555@gmail.com>
2026-05-23 14:20:16 +0200

    fix: Intentionally break add test for CI demo

--- calculator.test.js
+++ calculator.test.js
@@ -2,8 +2,8 @@
 const { add, subtract, multiply, divide } = require("./calculator");
 describe("Calculator", () => {
  test("adds 1 + 2 to equal 3", () => {
- expect(add(1, 2)).toBe(3);
- });
+ expect(add(1, 2)).toBe(5); // Intentionally changed to 5 to make it fail
+});
  test("subtracts 5 - 3 to equal 2", () => {
  expect(subtract(5, 3)).toBe(2);
  });
